ecab21c76d016c611dff19cc8daefe1b081f4b4a
Elsti97 <elsti.shehati@hotmail.it>
2022-11-11 17:29:40 +0100

    Inserimento prima immagine

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var app = new Vue(
+  {
+    el: '#root',
+    data:{
+      centrato: 'text-center mt-5 text-primary',
+      titolo: 'Slider',
+      image: 'https://picsum.photos/800/450?random=1'
+    }
+  }
+);
